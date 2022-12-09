c3f01f6ca6fe7eec9c0410de5322472eb0cbae4e
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-12-09 01:11:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+#include "lib.h"
+bool convert (char & a){
+    if (a>=65 and a<=90){
+        a +=32;
+        return true;
+    }else if(a>=97 and a<=122){
+        a -= 32;
+        return true;
+    }
+    return false;
+}
